a703da4ba63fbc46f7ab0e778bd414d937506fad
22sendai <kouhei.sendai@gmail.com>
2024-04-26 17:14:42 +0900

    init修正

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("入力"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -28,13 +28,16 @@ def response_generator():
         ChatMessage(role="system", content="あなたは日本人の優秀なアシスタントです。"),
         ChatMessage(role="user", content="日本語で答えることはできますか?"),
         ChatMessage(role="assistant", content="はい、日本語で丁寧にお答えします。"),
-        ChatMessage(role="user", content=prompt),
     ]
+    for conversation in st.session_state.messages:
+        messages.append(ChatMessage(role=conversation["role"], content=conversation["content"]))
+    messages.append(ChatMessage(role="user", content=prompt))
     response = st.session_state.llm.stream_chat(messages=messages)
     for chunk in response:
         yield chunk.delta
 
-with st.chat_message("assistant"):
-    response = st.write_stream(response_generator())
-# Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": response})
+if not len(st.session_state.messages) ==0:
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
